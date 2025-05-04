71be8f32599fb3721369e2e7c48f1637058cdf4b
sockenhandtuch <praxen-radios6@icloud.com>
2025-05-04 21:21:03 +0200

    added Drag and drop  dont work

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
-document.querySelectorAll('.chess-board td').forEach(cell => {
-    cell.addEventListener('click', () => {
-        alert('You clicked on a cell!');
+const draggables = document.querySelectorAll('.draggable');
+const dragContainer = document.querySelector('.drag-container');
+
+draggables.forEach(draggable => {
+    draggable.addEventListener('mousedown', (e) => {
+        let shiftX = e.clientX - draggable.getBoundingClientRect().left;
+        let shiftY = e.clientY - draggable.getBoundingClientRect().top;
+
+        const moveAt = (pageX, pageY) => {
+            draggable.style.left = pageX - shiftX + 'px';
+            draggable.style.top = pageY - shiftY + 'px';
+        };
+
+        const onMouseMove = (event) => {
+            moveAt(event.pageX, event.pageY);
+        };
+
+        document.addEventListener('mousemove', onMouseMove);
+
+        draggable.addEventListener('mouseup', () => {
+            document.removeEventListener('mousemove', onMouseMove);
+            draggable.onmouseup = null;
+
+            // Check if the draggable is within the drag container
+            const containerRect = dragContainer.getBoundingClientRect();
+            const draggableRect = draggable.getBoundingClientRect();
+
+            if (
+                draggableRect.left < containerRect.left ||
+                draggableRect.right > containerRect.right ||
+                draggableRect.top < containerRect.top ||
+                draggableRect.bottom > containerRect.bottom
+            ) {
+                // Reset position if dropped outside the container
+                draggable.style.left = '0px';
+                draggable.style.top = '0px';
+            }
+        });
     });
+
+    draggable.ondragstart = () => false; // Disable default drag behavior
 });
